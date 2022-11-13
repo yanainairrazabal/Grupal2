f4f0c26701ee2c6384d799d2dc7ab6626b0a7ffe
yanainairrazabal <110423905+yanainairrazabal@users.noreply.github.com>
2022-11-13 20:04:04 -0300

    Update 7-1
    -Modificaciones a los botones de modificar, agregar, eliminar.
    -Deshabilitarlos cuando no están los campos llenos.
    -API arreglada, pasa los datos de los usuarios ingresados.

--- main.js
+++ main.js
@@ -1,13 +1,41 @@
-const mockApi = "https://6363e9fb8a3337d9a2ec4742.mockapi.io/users/";
+const mockApi = "https://637160d507858778617bac42.mockapi.io/api/v1/users";
 const results = document.getElementById("results");
+var myModal = new bootstrap.Modal(document.getElementById('dataModal'), []);
 
 document.addEventListener("DOMContentLoaded", () => {
   listar();
+  document.querySelectorAll(".btn-disable").forEach(element => {
+    element.disabled  = true;
+  });
 });
 
 document.getElementById("btnSendChanges").addEventListener("click", () => {
     modificar();
 });
+
+document.querySelectorAll(".agregar").forEach((input) =>{
+   input.addEventListener("change", () => {
+        ableButton("btnPost", ".agregar");
+    }); 
+});
+
+document.querySelectorAll(".eliminar").forEach((input) =>{
+    input.addEventListener("change", () => {
+         ableButton("btnDelete",".eliminar");
+     }); 
+ });
+
+ document.querySelectorAll(".editar").forEach((input) =>{
+    input.addEventListener("change", () => {
+         ableButton("btnPut",".editar");
+     }); 
+ });
+
+function ableButton(btn, id)
+{
+    let btnAgregar = document.getElementById(btn);
+    btnAgregar.disabled = Array.prototype.some.call(document.querySelectorAll(id), (x) => x.value === "");
+}
 
 document.getElementById("btnGet1").addEventListener("click", () => {
     listar()
@@ -21,15 +49,15 @@ document.getElementById("btnPut").addEventListener("click", () => {
 function cargarModificar(){
     let id = document.getElementById("inputPutId").value;
 
-    fetch(mockApi + id).then((response) => {
+    fetch(mockApi + "/" + id).then((response) => {
         if (response.ok) {
             return response.json();
         }
         results.innerHTML = "El usuario no existe o ha ocurrido un error";
     }).then(data => {
-        document.getElementById("inputPutNombre").value = data.name;
-        document.getElementById("inputPutApellido").value = data.lastname;
-        var myModal = new bootstrap.Modal(document.getElementById('dataModal'), []);
+       
+        document.getElementById("inputPutNombre").value = data.nombre;
+        document.getElementById("inputPutApellido").value = data.apellido;
         myModal.show();
     }).catch((error) => {
         console.log(error);
@@ -39,19 +67,23 @@ function cargarModificar(){
 function listar() {
     let input = document.getElementById("inputGet1Id").value;
     results.innerHTML = "";
-
-    fetch(mockApi + input).then((response) => {
+    let url = mockApi;
+    if(input.length>0){
+        url +="/";
+    }
+    fetch(url + input).
+    then((response) => {
         if (response.ok) {
             return response.json();
-        }
+        } 
         results.innerHTML = "El usuario no existe o ha ocurrido un error";
     }).then(data => {
         if(Array.isArray(data)) {
             data.forEach(element => {
-                results.innerHTML += ` { "id": ${element.id},  "name": ${element.name}, "lastname": ${element.lastname} } <br>`;
+                results.innerHTML += ` { "id": ${element.id},  "nombre": ${element.nombre}, "apellido": ${element.apellido} } <br>`;
             });
         }else{
-            results.innerHTML = ` { "id": ${data.id},  "name": ${data.name}, "lastname": ${data.lastname} } <br>`;
+            results.innerHTML = ` { "id": ${data.id},  "nombre": ${data.nombre}, "apellido": ${data.apellido} } <br>`;
         }
     }).catch((error) => {
         console.log(error);
@@ -59,23 +91,24 @@ function listar() {
 }
 
 function agregar() {
-  let nombre = document.getElementById("inputPostNombre").value;
-  let apellido = document.getElementById("inputPostApellido").value;
+  let xnombre = document.getElementById("inputPostNombre").value;
+  let xapellido = document.getElementById("inputPostApellido").value;
 
   fetch(mockApi, {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json; charset=utf=8'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ "name": nombre, "lastname": apellido })
+    body: JSON.stringify({ nombre: xnombre, apellido: xapellido })
     }).then(response => {
         if (response.ok) {
-            return response.json();
+            results.innerHTML = `usuario agregado <br>`;
+            document.getElementById("inputPostNombre").value = "";
+            document.getElementById("inputPostApellido").value = "";
+            listar();
+            return;
         }
         results.innerHTML = "Ha ocurrido un error";
-    })
-    .then(data => {
-        results.innerHTML = ` { "id": ${data.id} } <br>`;
     }).catch((error) =>{console.log(error)});
 }
 
@@ -83,21 +116,31 @@ function modificar() {
     let id = document.getElementById("inputPutId").value;
     let nombre = document.getElementById("inputPutNombre").value;
     let apellido = document.getElementById("inputPutApellido").value;
-  
-    fetch(mockApi + id, {
+
+    fetch(mockApi + "/" + id, {
       method: 'PUT',
       headers: {
-        'Content-Type': 'application/json; charset=utf=8'
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ "name": nombre, "lastname": apellido })
-    })
-      .then(response => response.json())
-      .then(response => console.log(JSON.stringify(response)));
+      body: JSON.stringify({ "nombre": nombre, "apellido": apellido })
+    }).then(response2 => {
+        if (response2.ok) {
+            myModal.hide();
+            document.getElementById("inputPutId").value="";
+            document.getElementById("inputPutNombre").value="";
+            document.getElementById("inputPutApellido").value="";
+            listar();
+            return;
+        }
+        results.innerHTML = "Ha ocurrido un error";
+    }).catch((error) => {
+        console.log(error);
+    });
 }
 
 function eliminar() {
     let id = document.getElementById("inputDelete").value;
-    fetch(mockApi + id,{method:'DELETE'}).
+    fetch(mockApi + "/"+ id,{method:'DELETE'}).
     then(response => {
         if(response.ok) {
             listar();
